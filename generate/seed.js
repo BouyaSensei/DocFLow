f8bdd1914d7ab24cf4104add168ef0cbf2667620
BouyaSensei <buildario@outlook.fr>
2026-09-25 19:24:11 +0200

    Ajouter le script de génération de PDF via jsPDF

---
+++ generate/seed.js
@@ -0,0 +1,8 @@
+const { jsPDF } = require("jspdf");
+const boucle = 50;
+
+for (let i = 0; i <= boucle; i++) {
+  const doc = new jsPDF();
+  doc.text(`hello world ligne${i}`, 10, 10);
+  doc.save(`generate/${i}.pdf`);
+}
