1ff64d07b969148c63d5d8b190e28fd6c23d18ec
BouyaSensei <buildario@outlook.fr>
2026-09-25 19:24:31 +0200

    Corrige l'espacement manquant dans le texte des PDF générés

--- generate/seed.js
+++ generate/seed.js
@@ -3,6 +3,6 @@ const boucle = 50;
 
 for (let i = 0; i <= boucle; i++) {
   const doc = new jsPDF();
-  doc.text(`hello world ligne${i}`, 10, 10);
+  doc.text(`hello world ligne ${i}`, 10, 10);
   doc.save(`generate/${i}.pdf`);
 }
